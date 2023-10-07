e9b49fb3f019f4f5ec9a12101b784d6331419ea6
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2023-10-07 19:00:29 +0300

    entities are added 🪄

---
+++ DataAccess/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+namespace DataAccess;
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool IsActive { get; set; }
+
+    public Statuses Status { get; set; }
+
+    // Relations
+    // 1: One User can have one Role ---->
+    //        Please add the RoleId as well since it is on the ONE side and it needs to be distinguishable
+    public Role Role { get; set; }
+    public int RoleId { get; set; }
+
+    // 2: One User can have many UserResources
+    public List<UserResource> UserResources { get; set; }
+}
---
+++ DataAccess/Entities/UserResource.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess;
+
+public class UserResource
+{
+    // First Primary Key
+    [Key]
+    [Column(Order = 0)]
+    public int UserId { get; set; }
+
+    // Second Primary Key
+    [Key]
+    [Column(Order = 1)]
+    public int ResourceId { get; set; }
+
+    // Relations
+    // 1: One UserResource can have one User
+    public User User { get; set; }
+
+    // 2: One UserResource can have one Resource
+    public Resource Resource { get; set; }
+
+}
