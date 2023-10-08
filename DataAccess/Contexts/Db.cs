5e9f0f1e2df688105cbf83cf53e8bf7cc653652b
sumeyyeKurtulus <sumeyye.kurtulus.business@gmail.com>
2023-10-08 15:35:57 +0300

    Contexts are added 🪄

---
+++ DataAccess/Contexts/Db.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class Db: DbContext
+{
+
+    //1: Add the tables 
+    public DbSet<User> Users { get; set; }
+    public DbSet<Resource> Resources { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserResource> UserResources { get; set; }
+
+    public Db(DbContextOptions options): base(options){}
+
+    // We will need to create a model creation method for 
+    protected override void OnModelCreating(ModelBuilder modelBuilder){
+        modelBuilder.Entity<UserResource>().HasKey(e => new {e.UserId, e.ResourceId});
+    }
+}
